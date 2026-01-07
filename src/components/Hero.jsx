53e608c044d6126c868532dc69b8198df31d46fe
Moiz Inamdar <moizinamdar74@gmail.com>
2026-01-07 19:34:20 +0530

    Initial commit - PRO BODYLINE GYM

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,13 +17,24 @@
 // };
 
 // export default Hero;
-
-import { MdPadding } from "react-icons/md";
 import gymVideo from "../assets/gym.mp4";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => setHeight(window.innerHeight);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <section className="hero" id="home">
+    <section
+      className="hero"
+      id="home"
+      style={{ height: `${height}px` }}
+    >
       <video
         className="hero-video"
         autoPlay
@@ -36,9 +47,9 @@ const Hero = () => {
       </video>
 
       <div className="hero-content">
-        <h1></h1><br />
-        <a href="#about" className="hero-btn" >
-          Coming Soon...
+        <h1>NSP Gym</h1>
+        <a href="#about" className="hero-btn">
+          Coming Soon
         </a>
       </div>
     </section>
